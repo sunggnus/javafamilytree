fe0beded66a2382b701fa6d66c97ad478f8bc0b3
sunggnus <sunggnus@ee284a51-d3d3-4897-b903-1a318aafe125>
2013-04-16 20:51:10 +0000

    created the Sliders and extended the language entries only the listeners are still missing

--- trunk/src/tree/gui/field/ModifiedJSlider.java
+++ trunk/src/tree/gui/field/ModifiedJSlider.java
@@ -1,5 +1,7 @@
 package tree.gui.field;
 
+import java.awt.Dimension;
+
 import javax.swing.JSlider;
 
 public class ModifiedJSlider extends AbstractField{
@@ -9,8 +11,17 @@ public class ModifiedJSlider extends AbstractField{
 	 */
 	private static final long serialVersionUID = -6187568633787660247L;
 
-	public ModifiedJSlider(String name, int labelWidth, int fieldWidth){
+	public ModifiedJSlider(String name, int labelWidth){
 		super(name, labelWidth, new JSlider());
+		
+		this.setPreferredSize(new Dimension((int) this.getPreferredSize().getWidth(),
+				(int) this.getJSlider().getPreferredSize().getHeight()+10));
+	}
+	
+	public JSlider getJSlider(){
+		JSlider slider  = (JSlider) this.getFieldComponent();
+		
+		return slider;
 	}
 
 }
